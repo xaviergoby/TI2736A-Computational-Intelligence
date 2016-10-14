83076f842e48e327fd947768feb454c27560ee2f
Matthijs Kok <matthijs.kok@me.com>
2016-10-14 12:03:35 +0200

    Add evaporate() to Maze class.

--- src/Maze.java
+++ src/Maze.java
@@ -65,7 +65,11 @@ public class Maze {
      * @param rho evaporation factor
      */
     public void evaporate(double rho) {
-        ;
+    	for (int i = 0; i < pheromones.length; i++) {
+    		for (int j = 0; j < pheromones[i].length; i++) {
+    			pheromones[i][j] = (1 - rho) * pheromones[i][j];
+    		}
+    	}
     }
 
     /**
